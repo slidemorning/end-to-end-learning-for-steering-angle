bed3a98495d7b6b974ba0ca949307c191b5a778d
slidemorning <givertion2@gmail.com>
2019-11-28 13:58:19 +0900

    rename

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # end-to-end-learning-for-steering-angle
-End to end learning
+
+### Data Set  
+#####comma.ai driving dataset
+#####https://github.com/commaai/research
+
+### Reference
+
+#####https://github.com/commaai/research
+
--- utils.py
+++ utils.py
@@ -3,11 +3,9 @@ import random
 import h5py
 import cv2
 
-'''
-    # img   : 52722  (20Hz)
-    # label : 263583 (100Hz)
-'''
 
+# img   : 52722  (20Hz)
+# label : 263583 (100Hz)
 X_PATH = './data/image/image.h5'
 Y_PATH = './data/label/label.h5'
 TARGET_KEY_X = 'X'
@@ -19,16 +17,18 @@ def _down_sampling(data, hz_src, hz_dest):
     ret = []
     inc = hz_src//hz_dest
     for i in range(inc*10000, inc*20000, inc):
-        pass
+        ret.append(data[i])
+    return np.array(ret)
 
 
 def load_data(x_path, y_path):
     h5_x = h5py.File(x_path, 'r')
-    #h5_y = h5py.File(y_path, 'r')
+    h5_y = h5py.File(y_path, 'r')
     X = h5_x[TARGET_KEY_X][10000:20000]
     X = np.moveaxis(X, 1, -1)
-    #y = h5_y[TARGET_KEY_Y]
-    return X
+    y = h5_y[TARGET_KEY_Y]
+    y = _down_sampling(y, 100, 20)
+    return X, y
 
 # for debug
 def show_random_img(img_set):
@@ -36,10 +36,13 @@ def show_random_img(img_set):
     print('image at {}'.format(rand_num))
     cv2.imshow('img', img_set[rand_num])
     if cv2.waitKey(0) == 27:
-        return
+        return rand_num
 
 if __name__ == '__main__':
 
-    X = load_data(X_PATH, Y_PATH)
-    show_random_img(X)
-    #_down_sampling(1, 100, 20)
+    X, y = load_data(X_PATH, Y_PATH)
+    #rand_idx = show_random_img(X)
+    #print('label : ', y[rand_idx])
+    print(y.shape)
+    for val in y:
+        print(val)
