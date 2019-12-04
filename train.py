acd0defc459e8d47b89b34196cf8abf20958f38a
slidemorning <givertion2@gmail.com>
2019-12-04 14:18:05 +0900

    update callbacks

--- train.py
+++ train.py
@@ -1,13 +1,12 @@
 import utils
 import model
-
-### require : model check point
+import os
+from keras.callbacks import ModelCheckpoint
 
 # global var
-X_PATH = './drive/My Drive/img_preprocessed_v1.h5'
-Y_PATH = './drive/My Drive/label.h5'
-MODEL_SAVE_PATH = './steer-model/'
-MODEL_SAVE_NAME = 'v1_100.h5' # {X_data_ver}_{epochs}.h5
+X_PATH = './data/image/img_preprocessed_v1.h5'
+Y_PATH = './data/label/label.h5'
+MODEL_SAVE_FOLDER_PATH = './steer-model/'
 
 # hyperparameter of steer model
 EPOCHS = 100
@@ -20,18 +19,29 @@ if __name__ == '__main__':
     X, y = utils.shuffle_data(X, y)
 
     X_train, X_valid, X_test = X[:7000], X[7000:9000], X[9000:]
+
     y_train, y_valid, y_test = y[:7000], y[7000:9000], y[9000:]
 
     steer_model = model.get_model()
     steer_model.summary()
 
-    history = steer_model.fit(X_train, y_train,
-                        epochs=EPOCHS,
-                        batch_size=BATCH_SIZE,
-                        validation_data=(X_valid, y_valid),
-                        verbose=1)
+    if not os.path.exists(MODEL_SAVE_FOLDER_PATH):
+        os.mkdir(MODEL_SAVE_FOLDER_PATH)
+    model_path = MODEL_SAVE_FOLDER_PATH + '{epoch:04d}-{val_loss:.4f}.h5'
+    cb_checkpoint = ModelCheckpoint(filepath=model_path,
+                                    monitor='val_loss',
+                                    verbose=1,
+                                    save_best_only=True)
+
+    history = steer_model.fit(X_train,
+                              y_train,
+                              epochs=EPOCHS,
+                              batch_size=BATCH_SIZE,
+                              validation_data=(X_valid, y_valid),
+                              verbose=1,
+                              callbacks=[cb_checkpoint])
 
     utils.plot_history(history)
 
-    steer_model.save(MODEL_SAVE_PATH+MODEL_SAVE_NAME)
+    #steer_model.save(MODEL_SAVE_PATH+MODEL_SAVE_NAME)
 
